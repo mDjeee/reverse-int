5c8efeec87f29fb8898283d88ed484e9c66b3225
mDjeee <isejtniyazov@mail.ru>
2022-01-05 14:49:12 +0500

    feat: solve the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let one = Math.abs(n) % 10;
+    let ten = Math.abs(n) % 100 - Math.abs(n) % 10;
+    let hundred = Math.abs(n) - one - ten;
+    return one * 100 + ten + hundred / 100;
 }
